06a0d1e17a091fb2502bf1eff028e097a54e749f
sscovil <sscovil@gmail.com>
2025-05-17 10:31:03 -0400

    feat: export config types

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,11 @@
 export { WalletConnector } from './wallet-connector.js';
 export { AuthClient } from './auth-client.js';
-
+export type { AuthClientConfig } from './auth-client.js';
 export type {
     EIP1193Provider,
     EIP6963ProviderInfo,
     EIP6963ProviderDetail,
 } from './types.js';
-
 export type {
     EIP712Domain,
     EIP712AuthMessage,
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 export { AuthServer, createChallenge, verifyChallenge } from './auth-server.js';
-
+export type { AuthServerConfig } from './auth-server.js';
 export type {
     EIP712Domain,
     EIP712AuthMessage,
